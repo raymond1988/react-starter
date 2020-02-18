17884154baa57a88ea9db9e5cdb9c621936875e9
Raymond Manoharan <raymond_09@ymail.com>
2020-02-17 22:28:37 -0700

    First Commit Message

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <section className="text-center align-middle" >
+                <h1>This is the App.</h1>
+                <h6>Bootstrap Installed!</h6>
+                <p className="font-weight-light">Other ingredients - React-Redux, Axios, Redux-Thunk, Redux-Form</p>
+            </section>
+        )
+    }
+}
+
+
